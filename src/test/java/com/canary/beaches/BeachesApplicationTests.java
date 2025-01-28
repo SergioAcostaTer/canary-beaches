e85f53876d7e86f7d1a271bb81c557f8193de5fb
Sergio Acosta <sergio.acosta@bitboxonline.com>
2025-01-28 08:24:28 +0000

    first

---
+++ src/test/java/com/canary/beaches/BeachesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canary.beaches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeachesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
